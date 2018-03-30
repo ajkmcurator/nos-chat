43bb87c00f647a437b1b1f55aa7f7a46c6ccce97
[R3dact3d] <emmettwb@gmail.com>
2018-03-30 09:15:34 -0600

    Update newacct.js

--- static/newacct.js
+++ static/newacct.js
@@ -1,1 +1,31 @@
+// Dependencies
+var socket = io();
 
+// HTML Elements
+var user = document.getElementById("user"); // User input
+var key = document.getElementById("passkey"); // Passkey input
+var cKey = document.getElementById("confirmkey"); // Comfirm password input
+
+// Functions
+hashCode = function(str){
+    var hash = 0;
+    if (str.length == 0) return hash;
+    for (i = 0; i < str.length; i++) {
+        char = str.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+function sendRequest() {
+  uName = user.value;
+  pwd = key.value;
+  cPwd = key.value;
+  socket.emit('new account', 
+}
+
+// Callbacks
+socket.on('user check failed', function(data){
+  document.getElementById("err").InnerHTML = "Error: Username ["+data+"] is already in use!";
+});
