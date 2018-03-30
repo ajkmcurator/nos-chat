ce05b47ba2c8977bd6de16c591e26946ec8adb26
[R3dact3d] <emmettwb@gmail.com>
2018-03-30 11:24:02 -0600

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -143,6 +143,5 @@ socket.on('message', function(data){
         message = '<div>['+data[0]+'] '+data[1]+'</div>';
     }
     messages.innerHTML += message;
-    if (data[1] == '?kick '+uName) {socket.disconnect();}
     window.scrollTo(0,document.body.scrollHeight);
 });
